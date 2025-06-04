c787f5e28d93c138b92af0ea7e0abf92f51b81b9
BluesCoder2309 <singh.utkarsh2309@gmail.com>
2025-06-05 02:06:44 +0530

    first commit, an AI chat bot I made using google gemini API

---
+++ script.js
@@ -0,0 +1,75 @@
+const prompt = document.querySelector("#prompt");
+const chatContainer = document.querySelector(".chat-container");
+
+const apiURL =
+   "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyC89iRKhkWSch1nbNRTUKPRujuKoravK-U";
+let user = {
+   data: null,
+};
+
+async function generateResponse(aiChatBox) {
+   let text = aiChatBox.querySelector(".ai-chat-area");
+   let requestOption = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+         contents: [
+            {
+               parts: [
+                  {
+                     text: user.data,
+                  },
+               ],
+            },
+         ],
+      }),
+   };
+   try {
+      let response = await fetch(apiURL, requestOption);
+      let data = await response.json();
+
+      let apiResponse = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+      text.innerHTML = apiResponse;
+   } catch (error) {
+      console.log(error);
+   } finally {
+      chatContainer.scrollTo({ top: chatContainer.scrollHeight, behavior: "smooth" });
+   }
+}
+
+function createChatBox(html, classes) {
+   let div = document.createElement("div");
+   div.innerHTML = html;
+   div.classList.add(classes);
+   //return div, which is then caught by handleChatResponse when called.
+   return div;
+}
+
+function handleChatResponse(message) {
+   user.data = message; //in the object user the property data is null, but here it is assigned the value of message
+   let html = ` <img src="user.png" alt="" id="userImage" width="50" />
+            <div class="user-chat-area">
+                ${user.data}
+            </div>`;
+   prompt.value = "";
+   let userChatBox = createChatBox(html, "user-chat-box");
+   chatContainer.appendChild(userChatBox);
+   //to autoscroll when responses exceed the size of container
+   chatContainer.scrollTo({ top: chatContainer.scrollHeight, behavior: "smooth" });
+
+   setTimeout(() => {
+      let html = `<img src="ai.png" alt="" id="aiImage" width="70" />
+            <div class="ai-chat-area">
+              <img src="loading.webp" alt="" class="load" width='50px'>  
+            </div>`;
+      let aiChatBox = createChatBox(html, "ai-chat-box");
+      chatContainer.appendChild(aiChatBox);
+      generateResponse(aiChatBox);
+   }, 600);
+}
+
+prompt.addEventListener("keydown", (e) => {
+   if (e.key === "Enter") {
+      handleChatResponse(prompt.value);
+   }
+});
